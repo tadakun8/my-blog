5b4eec572797090f7c5ec3370e877e7afe17217e
tadakun8 <tadakun8.lab@gmail.com>
2022-03-11 17:26:18 +0900

    feat: Create list article page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,33 @@
-import type { NextPage } from "next";
-import { Box, chakra } from "@chakra-ui/react";
+import { NextPage, InferGetStaticPropsType } from "next";
+import { Box } from "@chakra-ui/react";
+import { getAllPosts } from "../lib/api";
 
-const Home: NextPage = () => {
-  return (
-    <Box>
-      <chakra.h1 color="tomato">Hello World</chakra.h1>
-    </Box>
-  );
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts(["slug", "title", "date", "tags"]);
+
+  return {
+    props: { allPosts },
+  };
 };
+
+const Home: NextPage<Props> = ({ allPosts }) => (
+  <ul>
+    {allPosts?.map((post) => (
+      <Box key={post.slug} mt={10}>
+        <li>
+          <h2>{post.title}</h2>
+          <p>{post.date}</p>
+          <ul>
+            {post.tags?.map((tag) => (
+              <li key={tag}>{tag}</li>
+            ))}
+          </ul>
+        </li>
+      </Box>
+    ))}
+  </ul>
+);
 
 export default Home;
