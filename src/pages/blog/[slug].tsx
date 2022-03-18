dc2069d88457a1e4b36f39279299be01d9bfdf53
tadakun8 <tadakun8.lab@gmail.com>
2022-03-18 18:27:16 +0900

    fix: Modify layout

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -37,38 +37,37 @@ export const getStaticProps = async ({ params }: any) => {
 
 const Post: NextPage<Props> = ({ post }) => (
   <>
-    <Flex h={100} justify="center" align="center">
-      <Heading as="h1" size="1.5xl">
-        {post.title}
-      </Heading>
-    </Flex>
-    <Flex justifyContent="space-between" alignItems="center">
-      <chakra.span fontSize="sm" color={useColorModeValue("gray.600", "gray.400")}>
-        {post.date}
-      </chakra.span>
-      <Box>
-        {post.tags.map((tag) => (
-          <Link
-            mx={1}
-            key={tag}
-            px={3}
-            py={1}
-            bg="gray.600"
-            color="gray.100"
-            fontSize="sm"
-            fontWeight="700"
-            rounded="md"
-            _hover={{ bg: "gray.500" }}
-          >
-            {tag}
-          </Link>
-        ))}
-      </Box>
-    </Flex>
-    <section>
-      {/* ここでdangerouslySetInnerHTMLを使ってHTMLタグを出力する */}
+    <Box px={40}>
+      <Flex h={100} justify="center" align="center">
+        <Heading as="h1" size="1.5xl">
+          {post.title}
+        </Heading>
+      </Flex>
+      <Flex justifyContent="space-between" alignItems="center">
+        <chakra.span fontSize="sm" color={useColorModeValue("gray.600", "gray.400")}>
+          {post.date}
+        </chakra.span>
+        <Box>
+          {post.tags.map((tag) => (
+            <Link
+              mx={1}
+              key={tag}
+              px={3}
+              py={1}
+              bg="gray.600"
+              color="gray.100"
+              fontSize="sm"
+              fontWeight="700"
+              rounded="md"
+              _hover={{ bg: "gray.500" }}
+            >
+              {tag}
+            </Link>
+          ))}
+        </Box>
+      </Flex>
       <div className="prose" dangerouslySetInnerHTML={{ __html: post.content }} />
-    </section>
+    </Box>
   </>
 );
 
