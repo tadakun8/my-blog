016d7968b2f79dbf3b3055dea1424df433cc7269
tadakun8 <tadakun8.lab@gmail.com>
2022-03-28 21:15:22 +0900

    feat: Show header on all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import Header from "../components/header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     // Specify resetCSS to false to prevent @tailwindcss/typography css from disappearing
     <ChakraProvider resetCSS={false}>
+      <Header />
       <Component {...pageProps} />
     </ChakraProvider>
   );
