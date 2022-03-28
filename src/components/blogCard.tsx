8bea200c1a8ca8eb24671bef53c8035f308958cd
tadakun8 <tadakun8.lab@gmail.com>
2022-03-28 21:12:54 +0900

    Modify blog card to be clickable.

--- src/components/blogCard.tsx
+++ src/components/blogCard.tsx
@@ -21,46 +21,44 @@ const BlogCard = (props: blogCardProps) => {
         bg={useColorModeValue("white", "gray.800")}
         maxW="2xl"
       >
-        <Flex justifyContent="space-between" alignItems="center">
-          <chakra.span fontSize="sm" color={useColorModeValue("gray.600", "gray.400")}>
-            {props.publishDate}
-          </chakra.span>
-          <Box>
-            {props.tags.map((tag) => (
-              <Link
-                mx={1}
-                key={tag}
-                px={3}
-                py={1}
-                bg="gray.600"
-                color="gray.100"
-                fontSize="sm"
-                fontWeight="700"
-                rounded="md"
-                _hover={{ bg: "gray.500" }}
-              >
-                {tag}
-              </Link>
-            ))}
-          </Box>
-        </Flex>
+        <NextLink href={`blog/${props.slug}`} passHref>
+          <Link
+            fontSize="2xl"
+            color={useColorModeValue("gray.700", "white")}
+            fontWeight="700"
+            _hover={{
+              color: useColorModeValue("gray.600", "gray.200"),
+              textDecoration: "none",
+            }}
+            _focus={{ outline: "none" }}
+          >
+            <Flex justifyContent="space-between" alignItems="center">
+              <chakra.span fontSize="sm" color={useColorModeValue("gray.600", "gray.400")}>
+                {props.publishDate}
+              </chakra.span>
+              <Box>
+                {props.tags.map((tag) => (
+                  <Link
+                    mx={1}
+                    key={tag}
+                    px={3}
+                    py={1}
+                    bg="gray.600"
+                    color="gray.100"
+                    fontSize="sm"
+                    fontWeight="700"
+                    rounded="md"
+                    _hover={{ bg: "gray.500" }}
+                  >
+                    {tag}
+                  </Link>
+                ))}
+              </Box>
+            </Flex>
 
-        <Box mt={2}>
-          <NextLink href={`blog/${props.slug}`} passHref>
-            <Link
-              fontSize="2xl"
-              color={useColorModeValue("gray.700", "white")}
-              fontWeight="700"
-              _hover={{
-                color: useColorModeValue("gray.600", "gray.200"),
-                textDecoration: "none",
-              }}
-              _focus={{ outline: "none" }}
-            >
-              {props.title}
-            </Link>
-          </NextLink>
-        </Box>
+            <Box mt={2}>{props.title}</Box>
+          </Link>
+        </NextLink>
       </Box>
     </Flex>
   );
