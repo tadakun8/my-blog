02f5ba0ba3014f20cb1a488c15c59ce9355b0533
tadakun8 <tadakun8.lab@gmail.com>
2022-04-02 16:24:46 +0900

    feat: Create Header menu icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,14 @@
-import { Box, Heading, HStack, Spacer, Link, useColorModeValue } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { Box, Heading, HStack, Spacer, Link, useColorModeValue, IconButton, VStack } from "@chakra-ui/react";
 import NextLink from "next/link";
+import { useState } from "react";
 
 const Header = () => {
+  const [isDisplayMenuList, setIsDisplayMenuList] = useState(false);
+
   return (
-    <Box mx={50} my={5}>
-      <HStack spacing={10}>
+    <VStack width="100%">
+      <HStack width="80%" height="5rem">
         <Heading>
           <NextLink href={"/"} passHref>
             <Link textDecoration="none" _hover={{ textDecoration: "none" }} _focus={{ outline: "none" }}>
@@ -13,13 +17,33 @@ const Header = () => {
             </Link>
           </NextLink>
         </Heading>
+
         <Spacer />
+
+        {/* Show button for mobile only */}
+        <IconButton
+          aria-label="Open menu"
+          icon={<HamburgerIcon />}
+          display={["flex", "flex", "none"]}
+          onClick={() => setIsDisplayMenuList(!isDisplayMenuList)}
+        />
+
+        {/* Show menu list only on desktop */}
+        <HStack spacing={8} display={["none", "none", "flex"]}>
+          <Box>menu01</Box>
+          <Box>menu02</Box>
+          <Box>menu03</Box>
+          <Box>menu04</Box>
+        </HStack>
+      </HStack>
+
+      <HStack spacing={5} display={isDisplayMenuList ? "flex" : "none"}>
         <Box>menu01</Box>
         <Box>menu02</Box>
         <Box>menu03</Box>
         <Box>menu04</Box>
       </HStack>
-    </Box>
+    </VStack>
   );
 };
 
