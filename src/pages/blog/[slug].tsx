daebe675cc03ac948ab58e44356d2a6d99955823
tadakun8 <tadakun8.lab@gmail.com>
2022-04-02 14:15:46 +0900

    style: Making article list and article page responsive

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -2,7 +2,7 @@ import { NextPage, InferGetStaticPropsType } from "next";
 import { getAllPosts, getPostBySlug } from "../../lib/api";
 import markdownToHtml from "../../lib/markdownToHtml";
 import "prismjs/themes/prism-okaidia.css";
-import { Box, chakra, Flex, Heading, Link, useColorModeValue } from "@chakra-ui/react";
+import { Box, chakra, Flex, Heading, HStack, Link, Spacer, useColorModeValue, VStack } from "@chakra-ui/react";
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
@@ -37,35 +37,38 @@ export const getStaticProps = async ({ params }: any) => {
 
 const Post: NextPage<Props> = ({ post }) => (
   <>
-    <Box px={40}>
-      <Flex h={100} justify="center" align="center">
+    <Box px={[0, 20, 40]}>
+      <VStack>
         <Heading as="h1" size="1.5xl">
           {post.title}
         </Heading>
-      </Flex>
-      <Flex justifyContent="space-between" alignItems="center">
-        <chakra.span fontSize="sm" color={useColorModeValue("gray.600", "gray.400")}>
-          {post.date}
-        </chakra.span>
-        <Box>
-          {post.tags.map((tag) => (
-            <Link
-              mx={1}
-              key={tag}
-              px={3}
-              py={1}
-              bg="gray.600"
-              color="gray.100"
-              fontSize="sm"
-              fontWeight="700"
-              rounded="md"
-              _hover={{ bg: "gray.500" }}
-            >
-              {tag}
-            </Link>
-          ))}
+        <Box width="100%">
+          <HStack>
+            <chakra.span fontSize="sm" color={useColorModeValue("gray.600", "gray.400")}>
+              {post.date}
+            </chakra.span>
+            <Spacer />
+            <Box>
+              {post.tags.map((tag) => (
+                <Link
+                  mx={1}
+                  key={tag}
+                  px={3}
+                  py={1}
+                  bg="gray.600"
+                  color="gray.100"
+                  fontSize="sm"
+                  fontWeight="700"
+                  rounded="md"
+                  _hover={{ bg: "gray.500" }}
+                >
+                  {tag}
+                </Link>
+              ))}
+            </Box>
+          </HStack>
         </Box>
-      </Flex>
+      </VStack>
       <div className="prose" dangerouslySetInnerHTML={{ __html: post.content }} />
     </Box>
   </>
